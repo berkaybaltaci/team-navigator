08d769301f81102d810afeb6f8aa342543867fd2
berkaybaltaci <berkaybaltaci@gmail.com>
2023-10-11 14:28:43 +0100

    First commit

---
+++ TeamNavigator/view/EmployeeProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  EmployeeProfile.swift
+//  TeamNavigator
+//
+//  Created by Berkay Baltaci on 11/10/2023.
+//
+
+import SwiftUI
+
+struct EmployeeProfileView: View {
+    var body: some View {
+      Text("SOME EMPLOYEE INFO")
+    }
+}
+
+struct EmployeeProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmployeeProfileView()
+    }
+}
