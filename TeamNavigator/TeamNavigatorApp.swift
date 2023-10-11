a78205460a880cec09e6328a592b1901d5ae4345
berkaybaltaci <berkaybaltaci@gmail.com>
2023-10-11 14:19:41 +0100

    Initial Commit

---
+++ TeamNavigator/TeamNavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamNavigatorApp.swift
+//  TeamNavigator
+//
+//  Created by Berkay Baltaci on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TeamNavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
